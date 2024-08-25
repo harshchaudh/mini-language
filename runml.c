af8e5101ced0467fe955c76845a7f65cd05f4491
harshchaudh <harsh.hc012@gmail.com>
2024-08-25 11:52:01 +0800

    Compile function and .gitignore

--- runml.c
+++ runml.c
@@ -2,6 +2,25 @@
 //  Student1:   23097928   Harsh Chaudhari
 //  Student2:   23087371   Jayden Hondris
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Compiles the given C file.
+ *
+ * Parameters:
+ *   filename - the file to be compiled
+ *
+ * Returns:
+ *   void
+ */
+void compile(char *filename)
+{
+    char command[100];
+    sprintf(command, "cc -o %s %s.c", filename, filename);
+    system(command);
+}
+
 int main(void)
 {
     return 0;
