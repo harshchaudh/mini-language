15a1ca20fe16fcf66366b2b3dd79ce5cdf37020d
harshchaudh <harsh.hc012@gmail.com>
2024-08-25 13:15:16 +0800

    Add platform information to runml.c

--- runml.c
+++ runml.c
@@ -1,6 +1,7 @@
 //  CITS2002 Project 1 2024
 //  Student1:   23097928   Harsh Chaudhari
 //  Student2:   23087371   Jayden Hondris
+//  Platform:   Linux
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,11 +18,17 @@
 void compile(char *filename)
 {
     char command[100];
-    sprintf(command, "cc -o %s %s.c", filename, filename);
+    sprintf(command, "cc -std=c11 -o %s %s.c", filename, filename);
     system(command);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        fprintf(stderr, "Error: Incorrect number of arguments.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        exit(EXIT_SUCCESS);
+    }
     return 0;
 }
