897d9ac385ce5b462a400c5047a68249c665cdae
harshchaudh <harsh.hc012@gmail.com>
2024-09-15 16:56:21 +0800

    Formatting fixed.

--- runml.c
+++ runml.c
@@ -17,15 +17,13 @@
 #define EXCLUDE_EXT 0        // Exclude the extension in the filename
 
 /** @brief A structure that defines a variable. */
-typedef struct variable
-{
+typedef struct variable {
     char name[IDENTIFIER_LENGTH];
     double value;
 } Variable;
 
 /** @brief A structure that defines a function. */
-typedef struct function
-{
+typedef struct function {
     char name[IDENTIFIER_LENGTH];
     Variable *args;
     int argCount;
@@ -34,8 +32,7 @@ typedef struct function
 } Function;
 
 /** @brief An enum that defines all the command types. */
-typedef enum commandType
-{
+typedef enum commandType {
     ASSIGNMENT,
     PRINT,
     FUNCTION_CALL,
@@ -44,8 +41,7 @@ typedef enum commandType
 } CommandType;
 
 /** @brief A structure that defines a command. */
-typedef struct command
-{
+typedef struct command {
     CommandType type;
     Function func;
     Variable var;
@@ -81,8 +77,7 @@ Command parseFunctionDefine(char *line)
 
     char *arg = strtok(argsBuffer, " ");
     command.func.argCount = 0;
-    while (arg != NULL)
-    {
+    while (arg != NULL) {
         arg = strtok(NULL, " ");
         command.func.argCount++; // Increment the argument count for that function
     }
@@ -90,8 +85,7 @@ Command parseFunctionDefine(char *line)
     command.func.args = malloc(command.func.argCount * sizeof(Variable));
     arg = strtok(argsBufferCopy, " ");
     int i = 0;
-    while (arg != NULL)
-    {
+    while (arg != NULL) {
         strcpy(command.func.args[i].name, arg);
         command.func.args[i].value = 0.0;
         arg = strtok(NULL, " ");
@@ -114,10 +108,8 @@ int getFunctionIndex(char *line, Command commandFunctions[], int functionCount)
     char functionName[IDENTIFIER_LENGTH];
     sscanf(line, "%[^(]", functionName);
 
-    for (int i = 0; i < functionCount; i++)
-    {
-        if (strcmp(commandFunctions[i].func.name, functionName) == 0)
-        {
+    for (int i = 0; i < functionCount; i++) {
+        if (strcmp(commandFunctions[i].func.name, functionName) == 0) {
             return i;
         }
     }
@@ -154,13 +146,12 @@ Command parseFunctionCall(char *line, Command command)
 char *getFilename(int includeExtension)
 {
     char *newFilename = malloc(12 * sizeof(char)); // ml-XXXXX.c
-    if (newFilename == NULL)
-    {
+    if (newFilename == NULL) {
         perror("Unable to allocate memory");
         exit(EXIT_FAILURE);
     }
 
-    //pid_t pid = getpid(); // Get the process ID (Apple, MacOS)
+    // pid_t pid = getpid(); // Get the process ID (Apple, MacOS)
     __pid_t pid = getpid(); // Get the process ID (Linux, Ubuntu)
 
     char pidString[6];             // PID is 5 digits long
@@ -169,8 +160,7 @@ char *getFilename(int includeExtension)
     strcpy(newFilename, "ml-");
     strcat(newFilename, pidString);
 
-    if (includeExtension)
-    {
+    if (includeExtension) {
         strcat(newFilename, ".c");
     }
 
@@ -188,29 +178,24 @@ char *getFilename(int includeExtension)
 void createFile(const char *newFilenameWithExt, Command commands[], int commandCount, Command functionCommands[], int functionCount)
 {
     FILE *file = fopen(newFilenameWithExt, "w");
-    if (file == NULL)
-    {
+    if (file == NULL) {
         fprintf(stderr, "Error: Unable to create file `%s`.\n", newFilenameWithExt);
         exit(EXIT_FAILURE);
     }
 
     fprintf(file, "#include <stdio.h>\n\n");
 
-    for (int i = 0; i < functionCount; i++)
-    {
+    for (int i = 0; i < functionCount; i++) {
         fprintf(file, "double %s(", functionCommands[i].func.name);
-        for (int j = 0; j < functionCommands[i].func.argCount; j++)
-        {
+        for (int j = 0; j < functionCommands[i].func.argCount; j++) {
             fprintf(file, "double %s", functionCommands[i].func.args[j].name);
-            if (j < functionCommands[i].func.argCount - 1)
-            {
+            if (j < functionCommands[i].func.argCount - 1) {
                 fprintf(file, ", ");
             }
         }
         fprintf(file, ") \n{\n");
 
-        if (functionCommands[i].type == FUNCTION_RETURN)
-        {
+        if (functionCommands[i].type == FUNCTION_RETURN) {
             fprintf(file, "\treturn %s;\n", functionCommands[i].func.returnVar);
         }
 
@@ -218,27 +203,18 @@ void createFile(const char *newFilenameWithExt, Command commands[], int commandC
     }
 
     fprintf(file, "int main() \n{\n");
-    for (int i = 0; i < commandCount; i++)
-    {
-        if (commands[i].type == ASSIGNMENT)
-        {
+    for (int i = 0; i < commandCount; i++) {
+        if (commands[i].type == ASSIGNMENT) {
             fprintf(file, "\tdouble %s = %f;\n", commands[i].var.name, commands[i].var.value);
-        }
-        else if (commands[i].type == PRINT)
-        {
+        } else if (commands[i].type == PRINT) {
             fprintf(file, "\tprintf(\"%%g\\n\", %s);\n", commands[i].var.name);
-        }
-        else if (commands[i].type == FUNCTION_CALL)
-        {
+        } else if (commands[i].type == FUNCTION_CALL) {
 
             fprintf(file, "\t%s(", commands[i].func.name);
-            if (commands[i].func.argCount > 0)
-            {
-                for (int j = 0; j < commands[i].func.argCount; j++)
-                {
+            if (commands[i].func.argCount > 0) {
+                for (int j = 0; j < commands[i].func.argCount; j++) {
                     fprintf(file, "%g", commands[i].func.args[j].value);
-                    if (j < commands[i].func.argCount - 1)
-                    {
+                    if (j < commands[i].func.argCount - 1) {
                         fprintf(file, ", ");
                     }
                 }
@@ -268,8 +244,7 @@ void compile(const char *filenameWithoutExt)
     sprintf(command, "cc -std=c11 -o %s %s.c", filenameWithoutExt, filenameWithoutExt);
     int result = system(command);
 
-    if (result != 0)
-    {
+    if (result != 0) {
         fprintf(stderr, "!Error: Compilation failed.\n");
         exit(EXIT_FAILURE);
     }
@@ -289,8 +264,7 @@ void run(const char *filenameWithoutExt)
     sprintf(command, "./%s", filenameWithoutExt);
     int result = system(command);
 
-    if (result != 0)
-    {
+    if (result != 0) {
         fprintf(stderr, "!Error: Execution failed.\n");
         exit(EXIT_FAILURE);
     }
@@ -320,8 +294,7 @@ void removeFile(char *filename)
 void removeComment(char *line)
 {
     char *comment = strchr(line, '#');
-    if (comment != NULL)
-    {
+    if (comment != NULL) {
         *comment = '\0';
     }
 }
@@ -343,37 +316,27 @@ void generateCode(const char *filename, const char *newFilenameWithExt)
     int functionIndex = -1;
 
     FILE *file = fopen(filename, "r");
-    if (file == NULL)
-    {
+    if (file == NULL) {
         fprintf(stderr, "Error: `%s` not found.\n", filename);
         exit(EXIT_FAILURE);
     }
 
     char line[MAX_LINE];
-    while (fgets(line, sizeof(line), file))
-    {
+    while (fgets(line, sizeof(line), file)) {
 
         removeComment(line);
 
-        if (strstr(line, "<-") != NULL)
-        {
+        if (strstr(line, "<-") != NULL) {
             commands[commandCount++] = parseAssignment(line);
-        }
-        else if (strstr(line, "print") != NULL)
-        {
+        } else if (strstr(line, "print") != NULL) {
             commands[commandCount++] = parsePrint(line);
-        }
-        else if (strstr(line, "function") != NULL)
-        {
+        } else if (strstr(line, "function") != NULL) {
             functionIndex = functionCount;
             commandFunctions[functionCount++] = parseFunctionDefine(line);
-        }
-        else if (strstr(line, "return") != NULL)
-        {
+        } else if (strstr(line, "return") != NULL) {
             commandFunctions[functionIndex] = parseFunctionReturn(line, commandFunctions[functionIndex]);
 
-        } else if ((strstr(line, "(") != NULL) && (strstr(line, ")") != NULL))
-        {   
+        } else if ((strstr(line, "(") != NULL) && (strstr(line, ")") != NULL)) {
             int functionIndex = getFunctionIndex(line, commandFunctions, functionCount);
             commands[commandCount++] = parseFunctionCall(line, commandFunctions[functionIndex]);
         }
@@ -400,8 +363,7 @@ void DEV_TOOL_REMOVE_ML(void)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
+    if (argc != 2) {
         fprintf(stderr, "Error: Incorrect number of arguments.\n");
         exit(EXIT_FAILURE);
     }
@@ -413,7 +375,7 @@ int main(int argc, char *argv[])
 
     generateCode(argv[1], newFilenameWithExt);
     compile(newFilenameWithoutExt);
-    
+
     run(newFilenameWithoutExt);
     /*
     removeFile(newFilenameWithoutExt);
